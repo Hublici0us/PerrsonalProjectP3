4155620340638c369148bc9b06fe76daa69b6e28
Jason Ky <744169@student.dusd.net>
2024-12-07 16:50:35 -0800

    added a few scripts for the chest need to work on the main menu

---
+++ JasonPersonalVRProject/Assets/ContainsObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public enum ToyType { dumbell, box, amogus}
+public class ContainsObject : MonoBehaviour
+{
+    public ToyType typeOfBox;
+    public int maxCount = 3;
+    public int toysInBox;
+    private int totalToys;
+    public TextMeshProUGUI toysText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalToys = 3;
+        UpdateAmount(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ToyScript findToy = other.gameObject.GetComponent<ToyScript>();
+        if (findToy.toyType == typeOfBox)
+        {
+            toysInBox++;
+            UpdateAmount(+1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ToyScript findToy = other.gameObject.GetComponent<ToyScript>();
+        if (findToy.toyType == typeOfBox)
+        {
+            toysInBox--;
+            UpdateAmount(-1);
+        }
+    }
+
+    public void UpdateAmount(int amount)
+    {
+        toysText.text = (toysInBox + "/" + totalToys);
+        Debug.Log(amount + typeOfBox);
+    }
+}
---
+++ JasonPersonalVRProject/Assets/ToyScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyScript : MonoBehaviour
+{
+    public ToyType toyType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
